b71bbd6d3c11cf91590fa2f81bd50aca1f899447
Ayaanle Nur <Ayaanle-n@hotmail.com>
2022-05-28 13:58:47 +0100

    each css is inside the component now

--- client/src/Components/Cards/Cards.js
+++ client/src/Components/Cards/Cards.js
@@ -1,10 +1,20 @@
 import React from "react";
-import './Cards.css'
+
+const styles = {
+  cardsContainer: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-evenly",
+    marginTop: "5vh",
+    paddingBottom: "5vh",
+    width: "94%"
+  }
+  }
 
 const Cards = (props) => {
   return (
     <div>
-      <ul class="cards-container">
+      <ul class="cards-container" style = {styles.cardsContainer}>
         {props.cards.map((card) => (
           <li className="card">
             {card.title}
--- client/src/Components/Rat/Rat.js
+++ client/src/Components/Rat/Rat.js
@@ -1,30 +1,61 @@
 import React from "react";
-import "./Rat.css";
+
+const styles = {
+  title: {
+    textAlign: "center",
+  },
+  card: {
+    textAlign: "center",
+    marginTop: "100px",
+  },
+  img: {
+    width: "20%",
+  },
+  cardTitle: {
+    fontSize: "25px",
+  },
+  input: {
+    width: "200px",
+    padding: "5px",
+    borderRadius: "5px",
+  },
+  btn: {
+    padding: "5px",
+    marginLeft: "10px",
+    cursor: "pointer",
+  },
+};
 
 const Rat = () => {
   return (
-    <div>
-        <div>
-            <h1 className="title">Adopt a Rat!</h1>
-        </div>
-        
-      <div className="container">
+    <div style={styles.title}>
+      <div>
+        <h1 className="title">Adopt A Rat!</h1>
+      </div>
+
+      <div className="container" style={styles.card}>
         <div className="card">
           <img
+            style={styles.img}
             className="card-img-top"
             src="https://static.vecteezy.com/system/resources/previews/003/304/312/original/a-sticker-template-of-rat-cartoon-character-free-vector.jpg"
             alt="rat eating cheese"
           />
           <div className="card-body">
-            <h5 className="card-title">Card Title</h5>
+            <h5 className="card-title" style={styles.cardTitle}>
+              Rattata
+            </h5>
           </div>
           <div>
             <input
+              style={styles.input}
               type="text"
               className="input"
               placeholder="Name Your Rat!"
             ></input>
-            <button className="btn">Submit</button>
+            <button className="btn" style={styles.btn}>
+              Submit
+            </button>
           </div>
         </div>
       </div>
