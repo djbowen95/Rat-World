9afb23dc9bef9ffb0e8ca6badfcf7e576873cc1b
Ayaanle Nur <Ayaanle-n@hotmail.com>
2022-05-31 19:23:18 +0100

    changed date to now

--- server/utils/diffHours.js
+++ server/utils/diffHours.js
@@ -1,13 +1,13 @@
-function diff_hours(dt2) 
+function diff_hours(date) 
  {
 
-  let diff =(dt2.getTime() - Date.now()) / 1000;
+  let diff =(date.getTime() - Date.now()) / 1000;
   diff /= (60 * 60);
   return Math.abs(Math.round(diff));
   
  }
 
-dt2 = new Date("May 31, 2022 11:49:00");
-console.log(diff_hours(dt2));
+date = new Date("May 31, 2022 11:49:00");
+console.log(diff_hours(date));
 
 module.exports = diffHours;
