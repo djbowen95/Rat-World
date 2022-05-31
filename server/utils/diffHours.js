f2f9ade461078563513770eb733555599e42f751
Ayaanle Nur <Ayaanle-n@hotmail.com>
2022-05-31 19:17:04 +0100

    diff hours.js in utils

---
+++ server/utils/diffHours.js
@@ -0,0 +1,15 @@
+function diff_hours(dt2, dt1) 
+ {
+
+  let diff =(dt2.getTime() - dt1.getTime()) / 1000;
+  diff /= (60 * 60);
+  return Math.abs(Math.round(diff));
+  
+ }
+
+
+dt1 = new Date("October 13, 2014 08:11:00");
+dt2 = new Date("October 13, 2014 11:11:00");
+console.log(diff_hours(dt1, dt2));
+
+module.exports = diffHours;
