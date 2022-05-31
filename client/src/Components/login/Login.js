d2689723bcee9118fc6d1b32f721b305d93ca0e3
Fatumoabdullahi <95045817+Fatumoabdullahi@users.noreply.github.com>
2022-05-31 20:08:09 +0100

    add signup div to react

--- client/src/Components/login/Login.js
+++ client/src/Components/login/Login.js
@@ -1,3 +1,24 @@
 import React from "react";
 //add function
+function login() {
+    return (
+        <div class="main">
+      <input type="checkbox" id="chk" aria-hidden="true" />
+
+      <div class="signup">
+        <form>
+          <label for="chk" aria-hidden="true">Sign up</label>
+          <input type="text" name="txt" placeholder="User name" required="" />
+          <input type="email" name="email" placeholder="Email" required="" />
+          <input
+            type="password"
+            name="pswd"
+            placeholder="Password"
+            required=""
+          />
+          <button>Sign up</button>
+        </form>
+      </div>
+    )
+}
 export default Footer;
