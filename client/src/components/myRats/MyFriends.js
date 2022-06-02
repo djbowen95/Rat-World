08d759b3b5d6f76d2521dbf8bbcd29f0225341d8
Ayaanle Nur <Ayaanle-n@hotmail.com>
2022-06-02 20:07:34 +0100

    added an add friends container

--- client/src/components/myRats/MyFriends.js
+++ client/src/components/myRats/MyFriends.js
@@ -1,43 +1,91 @@
-import React from 'react'
+import React from "react";
+import "../myRats/MyFriends.css";
 
 const MyFriends = () => {
+  const styles = {
+    flex: {
+      textAlign: "center",
+    },
+    friendsCards: {
+      display: "flex",
+      justifyContent: "space-evenly",
+      border: "2px solid",
+      boxShadow: "5px 5px gray",
+      padding: "10px 100px 10px 100px",
+    },
+    friendImage: {
+      border: "1px solid",
+      boxShadow: "5px 5px gray",
+      padding: "50px",
+      borderRadius: "50%",
+    },
+    friendTitle: {
+      textAlign: "center",
+    },
+    section: {
+      marginTop: "100px",
+      marginBottom: "100px",
+    },
+    addFriend: {
+      display: "flex",
+      justifyContent: "space-evenly",
+      border: "1px solid",
+      boxShadow: "5px 5px gray",
+      padding: "20px 100px 20px 100px",
+    },
+    input: {
+      width: "250px",
+      height: "30px",
+      marginRight: "10px",
+    },
+    btn: {
+      height: "30px",
+      cursor: "pointer",
+    },
+    inputBtn: {
+      marginTop: "40px",
+      marginLeft: "20px"
+    },
+  };
 
-    const styles = {
-        friendsCards: {
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-evenly",
-        },
-        friendImage: {
-            border: "2px solid",
-            padding: "60px",
-            borderRadius: "50%"
-        }
-    }
+  return (
+    <div style={styles.flex}>
+      <section className="friends-container">
+        <h2 style={styles.friendTitle}>My Friends</h2>
+        <div className="friends-cards" style={styles.friendsCards}>
+          <div className="single-cards">
+            <img style={styles.friendImage} src="" alt="" />
+            <h3>Username</h3>
+          </div>
 
-    return (
-        <div>
-            <section className="friends-container">
-                <h2>My Friends</h2>
-                <div className="friends-cards" style={styles.friendsCards}>
-                    <div className="single-cards">
-                        <img style={styles.friendImage} src="" alt="friend 1"/>
-                        <h3>Username</h3>
-                    </div>
+          <div>
+            <img style={styles.friendImage} src="" alt="" />
+            <h3>Username</h3>
+          </div>
 
-                    <div>
-                        <img style={styles.friendImage} src="" alt="friend 1"/>
-                        <h3>Username</h3>
-                    </div>
-
-                    <div>
-                        <img style={styles.friendImage} src="" alt="friend 1"/>
-                        <h3>Username</h3>
-                    </div>
-                </div>
-            </section>
+          <div>
+            <img style={styles.friendImage} src="" alt="" />
+            <h3>Username</h3>
+          </div>
         </div>
-    )
-}
+      </section>
 
-export default MyFriends
+      <section className="add-friend-container" style={styles.section}>
+        <h2 style={styles.friendTitle}>Add a Friend</h2>
+        <div className="add-friend-card" style={styles.addFriend}>
+          <img style={styles.friendImage} src="" alt="" />
+          <div style={styles.inputBtn}>
+            <input
+              style={styles.input}
+              className="input"
+              placeholder="Enter your email address..."
+            ></input>
+            <button style={styles.btn}>Submit</button>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default MyFriends;
