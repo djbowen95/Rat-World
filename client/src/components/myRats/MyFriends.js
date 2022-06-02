bdcfa7f9684e171e2f8bf58dc547498be85bab8a
Ayaanle Nur <Ayaanle-n@hotmail.com>
2022-06-02 15:34:25 +0100

    added 2 more cards to the friends container

--- client/src/components/myRats/MyFriends.js
+++ client/src/components/myRats/MyFriends.js
@@ -1,12 +1,37 @@
 import React from 'react'
 
 const MyFriends = () => {
+
+    const styles = {
+        friendsCards: {
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-evenly",
+        },
+        friendImage: {
+            border: "2px solid",
+            padding: "60px",
+            borderRadius: "50%"
+        }
+    }
+
     return (
         <div>
             <section className="friends-container">
-                <div className="friends-cards">
+                <h2>My Friends</h2>
+                <div className="friends-cards" style={styles.friendsCards}>
+                    <div className="single-cards">
+                        <img style={styles.friendImage} src="" alt="friend 1"/>
+                        <h3>Username</h3>
+                    </div>
+
                     <div>
-                        <img src="" alt="friend 1"/>
+                        <img style={styles.friendImage} src="" alt="friend 1"/>
+                        <h3>Username</h3>
+                    </div>
+
+                    <div>
+                        <img style={styles.friendImage} src="" alt="friend 1"/>
                         <h3>Username</h3>
                     </div>
                 </div>
