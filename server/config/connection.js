b862f933213b7c17a6cd478fb0f47b9d5a6e61f1
Ben <bgrowcott@hotmail.co.uk>
2022-06-11 14:27:43 +0100

    fixing main

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
-console.log(process.env.MONGODB_URI)
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/ratFriends", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
