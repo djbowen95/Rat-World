f44776fa23a3fc6b71740ee7743888ebe6dac19d
Daniel Bowen <djbowen95@gmail.com>
2022-06-14 21:55:30 +0100

    Bug fix on mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -3,10 +3,7 @@ import { gql } from "@apollo/client";
 export const CREATE_RAT = gql`
   mutation createRat($name: String!) {
     createRat(name: $name) {
-      user {
         _id
-        name
-      }
     }
   }
 `;
