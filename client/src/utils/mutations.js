0388dccd6aa1ac9f80a71806de026b9ad613c224
Daniel Bowen <djbowen95@gmail.com>
2022-06-16 16:53:00 +0100

    Update client mutation util to include bumIndex

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,8 +1,8 @@
 import { gql } from "@apollo/client";
 
 export const CREATE_RAT = gql`
-  mutation createRat($name: String!, $headIndex: Int!, $bodyIndex: Int!) {
-    createRat(name: $name, headIndex: $headIndex, bodyIndex: $bodyIndex) {
+  mutation createRat($name: String!, $headIndex: Int!, $bodyIndex: Int!, $bumIndex: Int!) {
+    createRat(name: $name, headIndex: $headIndex, bodyIndex: $bodyIndex, bumIndex: $bumIndex) {
         _id
     }
   }
