566714a7b5050e9aac7bc01cb9195b4fcfeac0e2
Daniel Bowen <djbowen95@gmail.com>
2022-06-17 18:22:51 +0100

    Transfer HTML in frontend-draft  to JSX in viewMyRats and ratCards

--- client/src/components/myRats/viewMyRats/viewMyRats.js
+++ client/src/components/myRats/viewMyRats/viewMyRats.js
@@ -4,8 +4,6 @@ import { useQuery } from "@apollo/client";
 import { QUERY_RATS } from "../../../utils/queries";
 
 import RatCard from "./ratCards";
-import Fab from "@mui/material/Fab";
-import AddIcon from "@mui/icons-material/Add";
 
 function ViewMyRats() {
   const styles = {
@@ -14,42 +12,24 @@ function ViewMyRats() {
       justifyContent: "space-around",
       alignItems: "center",
     },
-    adoptRat: {
-      display: "flex",
-      justifyContent: "space-around",
-      alignItems: "center",
-    },
+
   };
 
   const { loading, data } = useQuery(QUERY_RATS);
   const rats = data?.rats || [];
 
   return (
-    <div className="innerBox">
-      <div style={styles.heading}>
-        <h2>My Rats</h2>
-        <div style={styles.adoptRat}>
-          <Fab
-            variant="extended"
-            size="medium"
-            color="primary"
-            aria-label="add"
-          >
-            Adopt a Rat
-            <AddIcon />
-          </Fab>
-        </div>
-      </div>
+    <div>
+    <header class="header">
+      <h1 class="title">My Rats</h1>
+      <button class="adopt-btn">+ Adopt a rat</button>
+    </header>
+
+    <ul class="card-wrapper">
       <div>{rats.map((rat) => (<RatCard rat={rat}/>))}</div>
+      </ul>
     </div>
   );
-}
-
-// {inventory?.map((item, index) => (
-//   <div style={styles.item} key={index}>
-//     <div>{item.itemName}</div>
-//     <div title={item.description}><img style={styles.img} src={getImage(item.image)}/></div>
-//   </div>
-// ))}
+};
 
 export default ViewMyRats;
